95b3a316b88097b0c2f935534a0cd2c2e7ed40eb
MAREEDU ARAVINDH <127046280+Aravindh-4195@users.noreply.github.com>
2024-08-06 19:39:23 +0530

    Update index.js

--- sample/server/index.js
+++ sample/server/index.js
@@ -14,7 +14,7 @@ app.use(cors({
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://1mareeduaravindh1:mongo123@cluster0.esgzz6v.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0');
 
 app.get("/", (req, res) => {
   res.json("Hello");
